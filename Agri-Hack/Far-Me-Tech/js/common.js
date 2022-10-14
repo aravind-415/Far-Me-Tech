73ea823a413d36142b9542d84f095916b7da0efe
aravind-415 <115813455+aravind-415@users.noreply.github.com>
2022-10-14 22:17:43 +0530

    Add files via upload

---
+++ Agri-Hack/Far-Me-Tech/js/common.js
@@ -0,0 +1,11 @@
+const home=document.getElementById("home");
+const weather=document.getElementById("weather");
+const prices=document.getElementById("prices");
+const shop=document.getElementById("shop");
+const experts=document.getElementById("experts");
+const login=document.getElementById("login");
+const admin=document.getElementById("admin");
+
+home.addEventListener("click",()=>{
+    window.open(weather);
+};
